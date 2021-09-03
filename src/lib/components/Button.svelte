c227ce8e81a072c5303700f9a53232224cd8aa0c
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 14:31:23 +0430

    Create button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	type ButtonType = "button" | "submit"
+
+	export let type: ButtonType = "button"
+	export let disabled = false
+</script>
+
+<button
+	{type}
+	{disabled}
+	class="p-2 rounded bg-blue-500 text-white transition focus:bg-blue-600 disabled:bg-gray-400 disabled:cursor-wait"
+>
+	<slot />
+</button>
