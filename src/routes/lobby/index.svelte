df4da6f41e79f77d7f74ed0e3fcfed03cbd4587d
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 18:24:05 +0430

    Add lobby route

---
+++ src/routes/lobby/index.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { Schema } from "$lib/schemas"
+	import { onMount } from "svelte"
+	import * as navigation from "$app/navigation"
+	import { Cookies } from "$lib/utils"
+	import { Lobby } from "$lib/api"
+
+	let lobbies: Schema.Lobby[] = []
+
+	onMount(async () => {
+		lobbies = await Lobby.list()
+	})
+
+	const onJoinLobby = async (uuid: string) => {
+		const lobby = await Lobby.join(uuid)
+		navigation.goto(`/lobby/${lobby.id}`)
+	}
+
+	const onLogout = () => {
+		Cookies.remove("access-token")
+		navigation.goto("/")
+	}
+
+	const onNewLobby = async () => {
+		const lobby = await Lobby.create()
+		navigation.goto(`/lobby/${lobby.id}`)
+	}
+</script>
+
+<main class="p-10 h-screen flex flex-col justify-between gap-y-10">
+	<div class="flex flex-col gap-4 overflow-auto px-3">
+		{#each lobbies as lobby}
+			<div class="flex justify-between items-stretch bg-gray-100 rounded">
+				<span class="pl-6 self-center">
+					{lobby.host.username}
+				</span>
+				<button on:click={() => onJoinLobby(lobby.uuid)} class="btn-blue p-4">Join</button>
+			</div>
+		{/each}
+	</div>
+
+	<div class="flex justify-between">
+		<button on:click={onLogout} class="btn-red w-28">Logout</button>
+		<button on:click={onNewLobby} class="btn-blue w-28">New Lobby</button>
+	</div>
+</main>
