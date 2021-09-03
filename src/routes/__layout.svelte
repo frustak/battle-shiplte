996f8461f49ffacfc5f67e1ea9127bb64a63ca3a
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 21:05:59 +0430

    Add check user signed in

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
 	import "../app.postcss"
+	import { onMount } from "svelte"
+	import * as navigation from "$app/navigation"
+	import { User } from "$lib/api"
+
+	let signedIn = false
+
+	onMount(async () => {
+		try {
+			await User.check_token()
+			signedIn = true
+			navigation.goto("/lobby")
+		} catch {
+			signedIn = true
+			navigation.goto("/")
+		}
+	})
 </script>
 
 <svelte:head>
 	<title>Battle Shiplte</title>
 </svelte:head>
 
-<div class="text-gray-900">
-	<slot />
-</div>
+{#if signedIn}
+	<div class="text-gray-900">
+		<slot />
+	</div>
+{:else}
+	Loading...
+{/if}
