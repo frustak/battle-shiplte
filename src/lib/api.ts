2f894161631fb477c14bae3ff26dfde75e064b18
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 12:01:19 +0430

    Add user create api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,3 +1,22 @@
 import ky from "ky"
 
-export const api = ky.create({ prefixUrl: import.meta.env.VITE_API_ADDRESS })
+const api = ky.create({ prefixUrl: import.meta.env.VITE_API_ADDRESS })
+
+export namespace User {
+	type User = {
+		id: number
+		username: string
+	}
+
+	type Token = {
+		access_token: string
+		token_type: string
+		user: User
+	}
+
+	export async function create(formValues: { username: string }): Promise<Token> {
+		const token: Token = await api.post("token", { json: formValues }).json()
+
+		return token
+	}
+}
