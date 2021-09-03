33639df08e13cb36f4f78cb4fcb4e6961c9aa371
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 22:40:26 +0430

    Add on interval lifecycle

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./form"
 export * from "./cookies"
+export * from "./on-interval"
---
+++ src/lib/utils/on-interval.ts
@@ -0,0 +1,11 @@
+import { onDestroy, onMount } from "svelte"
+
+export function onInterval(callback: () => void, milliseconds = 5000): void {
+	onMount(callback)
+
+	const interval = setInterval(callback, milliseconds)
+
+	onDestroy(() => {
+		clearInterval(interval)
+	})
+}
