159c70ed1b39be26e8b4c97cd919a8b882f6dcb7
Erfan Qasemi Zade <erfan.qasemizade@utopiops.com>
2021-09-03 14:31:32 +0430

    Add lobby type

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,18 +2,27 @@ import ky from "ky"
 
 const api = ky.create({ prefixUrl: import.meta.env.VITE_API_ADDRESS })
 
+export type User = {
+	id: number
+	username: string
+}
+
+export type Token = {
+	access_token: string
+	token_type: string
+	user: User
+}
+
+export type Lobby = {
+	id: number
+	uuid: string
+	host: User
+	players: User[]
+	has_started: boolean
+	is_full: boolean
+}
+
 export namespace User {
-	type User = {
-		id: number
-		username: string
-	}
-
-	type Token = {
-		access_token: string
-		token_type: string
-		user: User
-	}
-
 	export async function create(formValues: { username: string }): Promise<Token> {
 		const token: Token = await api.post("token", { json: formValues }).json()
 
